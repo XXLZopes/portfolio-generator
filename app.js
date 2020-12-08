7bfc1fdf47f5c9f03ff6d1e8b6d01385705d9ae9
XXLZopes <afordm99@gmail.com>
2020-12-07 19:49:22 -0800

    created web page

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
-const profileDataArgs = process.argv.slice(2, process.argv.length);
 
-const printProfileData = profileDataArr =>{
-    profileDataArr.forEach(profileItem => console.log(profileItem));
-};
+const fs = require('fs');
 
-printProfileData(profileDataArgs);
+const generatePage = require('./src/page-template.js');
+
+const profileDataArgs = process.argv.slice(2);
+
+const [name, github] = profileDataArgs;
+//Three inputs for fs.writeFile() 1st is the file name, 
+//2nd is the data that is being written (HTML string),
+//3rd is the callback function that will handle any errors and success massage.
+fs.writeFile('./index.html', generatePage(name, github), err =>{
+    if (err) throw err;
+    console.log('Portfolio complete! Check out index.html to see the output!');
+});
